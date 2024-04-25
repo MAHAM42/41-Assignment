fc24e865d611e80958eac29c8fe86a31480a71c1
Maham Shahid <161121868+MAHAM42@users.noreply.github.com>
2024-04-25 09:50:46 -0700

    41-Assignment
    TypeScript 45-Assignment of Q41 with Node.js

---
+++ main41.ts
@@ -0,0 +1,19 @@
+// ASSIGNMENT NO 41:
+
+/*
+Magicians: Make a array of magician’s names.
+Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+*/
+
+console.log("ASSIGNMENT NO 41:");
+
+// Define a function to print each magician name from an array:
+function show_magicians(magicians: string[]) {
+    magicians.forEach(name => console.log(name));
+}
+
+// Define an array containing magicians name:
+let magicians_names = ["Harry Poter", "Maham", "Uzma"]
+
+// Call the function to print each magician name:
+show_magicians(magicians_names); 
